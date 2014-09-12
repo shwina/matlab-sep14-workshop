83bc55452fbb93bba44b23a548a1fee06e477c53
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-09-12 08:07:30 -0400

    fixed analyze.m to handle img name automatically

--- analyze.m
+++ analyze.m
@@ -2,25 +2,13 @@ for i = 1:3
     
     % Generate strings for file and image names:
     file_name = sprintf('inflammation-%02d.csv', i);
-    img_name = sprintf ('patient_data-%02d.jpg', i);
-
+    img_name = strcat(file_name(1:end-3), 'jpg')
+    
     patient_data = csvread(file_name);
     ave_inflammation = mean(patient_data, 1);
 
     figure()
-
-    subplot(1, 3, 1);
     plot(ave_inflammation);
-    ylabel('average')
-
-    subplot(1, 3, 2);
-    plot(max(patient_data, [], 1));
-    ylabel('max')
-
-    subplot(1, 3, 3);
-    plot(min(patient_data, [], 1));
-    ylabel('min')
-
     print(img_name);
     close();
 
--- test_range_overlap.m
+++ test_range_overlap.m
@@ -3,8 +3,7 @@
 
 % assert(range_overlap([0.0, 1.0], [5.0, 6.0]) == NaN);
 % assert(range_overlap([0.0, 1.0], [1.0, 2.0]) == NaN);
+
 assert(all(range_overlap([0, 1.0]) == [0, 1.0]));
 assert(all(range_overlap([2.0, 3.0], [2.0, 4.0]) == [2.0, 3.0]));
 assert(all(range_overlap([0.0, 1.0], [0.0, 2.0], [-1.0, 1.0]) == [0.0, 1.0]));
-
-
